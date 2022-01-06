549ad6e21f5342e4b3130275641d29a1b9e02a0f
Arturas <{ID}+{username}@users.noreply.github.com>
2022-01-06 17:20:34 +0300

    Version 0.0.1 | added formInput class

---
+++ src/formInput.js
@@ -0,0 +1,15 @@
+export class FormInput {
+    constructor(id, labelText, type) {
+        this.inputWrap = document.createElement('div');
+        this.inputLabel = document.createElement('label');
+        this.inputComponent = document.createElement('input');
+        this.inputComponent.id = id;
+        this.inputLabel.setAttribute('for', this.inputComponent.id);
+        this.inputLabel.innerText = labelText;
+        if(type) {
+            this.inputComponent.type = type;
+        }
+        this.inputWrap.appendChild(this.inputLabel);
+        this.inputWrap.appendChild(this.inputComponent);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import { Popup } from "./popup.js";
-if(window) {
-    window.uikit = { Popup };
-}
-export { Popup } from "./popup.js";
+import { FormInput } from "./formInput.js";
+import { myFunc } from "./myFunc.js";
+//if(window) {
+//    window.uikit = { Popup, FormInput };
+//}
+//const MY_CONST = Popup;
+//function myFunc() {};
+export { myFunc };
